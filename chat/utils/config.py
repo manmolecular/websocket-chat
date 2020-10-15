9aa4de4132572f070f81e8a2c3544e8648315d37
manmolecular <regwebghost@yandex.ru>
2020-10-15 23:38:08 +0700

    Move default db values into the config file

--- chat/utils/config.py
+++ chat/utils/config.py
@@ -1,3 +1,4 @@
+from os import environ
 from pathlib import Path
 from random import choices
 from string import ascii_uppercase, digits
@@ -7,6 +8,14 @@ class DefaultPaths:
     STATIC = Path(__file__).parent.parent.joinpath("static")
     STATIC_TEMPLATES = STATIC.joinpath("templates")
     STATIC_JS = STATIC.joinpath("js")
+
+
+class DbValues:
+    POSTGRES_DATABASE = environ.get("POSTGRES_DATABASE", default="chat")
+    POSTGRES_PASSWORD = environ.get("POSTGRES_PASSWORD", default="pass")
+    POSTGRES_USER = environ.get("POSTGRES_USER", default="user")
+    POSTGRES_HOST = environ.get("POSTGRES_HOST", default="localhost")
+    POSTGRES_PORT = environ.get("POSTGRES_PORT", default="5432")
 
 
 class JWTConfiguration:
