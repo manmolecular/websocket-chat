fe59636569793eb6e6939ac5eb235ab7389c701c
manmolecular <regwebghost@yandex.ru>
2020-10-16 19:21:07 +0700

    Templates update, JS refactoring

--- chat/static/js/chat.js
+++ chat/static/js/chat.js
@@ -2,7 +2,13 @@ let accessToken = null;
 
 var defaultFetch = fetch;
 
-fetch = function(url, options) {
+/**
+ * Patch fetch to include additional 'Authorization' header
+ * @param  {String} url      Original URL
+ * @param  {Object} options  Original options
+ * @return {Promise}
+ */
+fetch = function(url, options = {}) {
     if (accessToken !== null) {
         if (options['headers'] !== undefined) {
             options['headers']['Authorization'] = `Bearer ${accessToken}`;
@@ -15,7 +21,12 @@ fetch = function(url, options) {
     return defaultFetch(url, options);
 }
 
-loadPage = function(page) {
+/**
+ * Get data from API ('/page'), update and navigate to '/#page'
+ * @param  {String} page     page URL
+ * @return {null}
+ */
+loadPage = function(page = '') {
     const container = document.getElementById('home-container');
     const options = {
         method: 'GET',
@@ -28,7 +39,13 @@ loadPage = function(page) {
         });
 }
 
-showStatus = function(parent, message) {
+/**
+ * Add status from API, add to the parent block
+ * @param  {String} parent     parent HTML element to append
+ * @param  {String} message    status message
+ * @return {null}
+ */
+showStatus = function(parent, message = 'undefined') {
     const container = document.getElementById(parent);
     const element = document.createElement('div');
     element.innerHTML = message + ', go to the <a href="#home">home page</a>'
@@ -36,78 +53,85 @@ showStatus = function(parent, message) {
 }
 
 window.addEventListener('hashchange', function(event) {
-    if (window.location.hash === '#home') {
-        loadPage('');
-    }
-    if (window.location.hash === '#chat') {
-        const ws = `ws://${window.location.hostname}:${window.location.port}/api/chat/ws`;
-        const container = document.getElementById('chat-container');
-        const socket = new WebSocket(ws);
-        socket.onmessage = function(event) {
-            const element = document.createElement('div');
-            message = JSON.parse(event.data);
-            const text = document.createTextNode(`${message.user} (${message.time}): ${message.message}`);
-            element.appendChild(text);
-            container.appendChild(element);
-        };
-        socket.onclose = function(event) {
-            const element = document.createElement('div');
-            const text = document.createTextNode('Websocket closed. Please reload.');
-            element.appendChild(text);
-            element.setAttribute('style', 'color:red;');
-            container.appendChild(element);
-        };
-        document.getElementById('send-button').addEventListener('click', function() {
-            const messageFieldElem = document.getElementById('message-field');
-            message = JSON.stringify({'message': messageFieldElem.value, 'token': accessToken});
-            socket.send(message);
-        });
-    }
-    if (window.location.hash === '#register') {
-        window.document.getElementById('register-button').addEventListener('click', function() {
-            const username = document.getElementById('username').value;
-            const password = document.getElementById('password').value;
-            const options = {
-                method: 'POST',
-                body: JSON.stringify({
-                    'username': username,
-                    'password': password,
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+    switch (window.location.hash) {
+        case '#home':
+            loadPage();
+            break;
+        case '#chat':
+            const ws = `ws://${window.location.hostname}:${window.location.port}/api/chat/ws`;
+            const container = document.getElementById('chat-container');
+            const socket = new WebSocket(ws);
+            socket.onmessage = function(event) {
+                const element = document.createElement('div');
+                const jsonMessage = JSON.parse(event.data);
+                const textMessage = document.createTextNode(`${jsonMessage.user} (${jsonMessage.time}): ${jsonMessage.message}`);
+                element.appendChild(textMessage);
+                container.appendChild(element);
             };
-            fetch('/api/register', options)
-                .then((res) => res.json())
-                .then((json) => {
-                    if (json.status === 'success') {
-                        showStatus('register-container', json.message);
-                    }
-                });
-        });
-    }
-    if (window.location.hash === '#login') {
-        document.getElementById('login-button').addEventListener('click', function() {
-            const username = document.getElementById('username').value;
-            const password = document.getElementById('password').value;
-            const options = {
-                method: 'POST',
-                body: JSON.stringify({
-                    'username': username,
-                    'password': password,
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+            socket.onclose = function(event) {
+                const element = document.createElement('div');
+                const textMessage = document.createTextNode('Websocket closed. Please reload.');
+                element.appendChild(textMessage);
+                element.setAttribute('style', 'color:red;');
+                container.appendChild(element);
             };
-            fetch('/api/login', options)
-                .then((res) => res.json())
-                .then((json) => {
-                    if (json.status === 'success') {
-                        accessToken = json.token;
-                        showStatus('login-container', json.message);
-                    }
+            document.getElementById('send-button').addEventListener('click', function() {
+                const messageFieldElem = document.getElementById('message-field');
+                const jsonMessage = JSON.stringify({
+                    'message': messageFieldElem.value,
+                    'token': accessToken
                 });
-        });
+                socket.send(jsonMessage);
+            });
+            break;
+        case '#register':
+            window.document.getElementById('register-button').addEventListener('click', function() {
+                const username = document.getElementById('username').value;
+                const password = document.getElementById('password').value;
+                const options = {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        'username': username,
+                        'password': password,
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                };
+                fetch('/api/register', options)
+                    .then((res) => res.json())
+                    .then((json) => {
+                        if (json.status === 'success') {
+                            showStatus('register-container', json.message);
+                        }
+                    });
+            });
+            break;
+        case '#login':
+            document.getElementById('login-button').addEventListener('click', function() {
+                const username = document.getElementById('username').value;
+                const password = document.getElementById('password').value;
+                const options = {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        'username': username,
+                        'password': password,
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                };
+                fetch('/api/login', options)
+                    .then((res) => res.json())
+                    .then((json) => {
+                        if (json.status === 'success') {
+                            accessToken = json.token;
+                            showStatus('login-container', json.message);
+                        }
+                    });
+            });
+            break;
+        default:
+            loadPage();
     }
 });
