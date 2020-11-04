1e1402dc6886b25df53ef144e3fff542cd24b6d3
manmolecular <regwebghost@yandex.ru>
2020-11-04 17:57:28 +0700

    Update JS

--- chat/static/js/chat.js
+++ chat/static/js/chat.js
@@ -1,5 +1,28 @@
-let accessToken = null;
+/**
+ * Create token storage closure
+ */
+var tokenStorage = function() {
+    var accessToken = null;
+    return {
+        /**
+         * Set token variable
+         * @param {String} token    access token
+         * @return {null}
+         */
+        setToken: function(token) {
+            accessToken = token;
+        },
+        /**
+         * Get token variable
+         * @return {String} token   access token
+         */
+        getToken: function() {
+            return accessToken;
+        }
+    };
+}();
 
+// Patch original fetch function to include auth header
 var defaultFetch = fetch;
 
 /**
@@ -8,7 +31,8 @@ var defaultFetch = fetch;
  * @param  {Object} options  Original options
  * @return {Promise}
  */
-fetch = function(url, options = {}) {
+var fetch = function(url, options = {}) {
+    accessToken = tokenStorage.getToken();
     if (accessToken !== null) {
         if (options['headers'] !== undefined) {
             options['headers']['Authorization'] = `Bearer ${accessToken}`;
@@ -26,7 +50,7 @@ fetch = function(url, options = {}) {
  * @param  {String} page     page URL
  * @return {null}
  */
-loadPage = function(page = '', method = 'GET', prefix = '') {
+var loadPage = function(page = '', method = 'GET', prefix = '') {
     const options = {
         method: method,
     };
@@ -47,36 +71,54 @@ loadPage = function(page = '', method = 'GET', prefix = '') {
 }
 
 /**
+ * Add self-made enter listener (because we don't use "submit" listener)
+ * @param  {String} element      element to click on
+ * @param  {String} container    container to check events
+ * @return {null}
+ */
+var enterListener = function(element, container) {
+    window.document.getElementById(container).addEventListener('keyup', function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById(element).click();
+        }
+    });
+}
+
+/**
  * Add status from API, add to the parent block
  * @param  {String} parent     parent HTML element to append
  * @param  {String} message    status message
  * @return {null}
  */
-showStatus = function(parent, message = 'undefined') {
+var showStatus = function(parent, message = 'undefined', additional = 'go to the <a href="#home">home page</a>') {
     const container = document.getElementById(parent);
     const element = document.createElement('div');
-    element.innerHTML = message + ', go to the <a href="#home">home page</a>'
+    element.innerHTML = message + ', ' + additional;
     container.appendChild(element);
 }
 
 window.addEventListener('hashchange', function(event) {
     switch (window.location.hash) {
         case '#home':
+            // Load main page
             loadPage();
             break;
         case '#logout':
-            accessToken = null;
+            // Remove token, load main page
+            tokenStorage.setToken(null);
             loadPage();
             break;
         case '#feedback':
-            // nothing
+            // Follow feedback url, API usage not required (static)
             break;
         case '#chat':
+            // Create WebSocket connection
             const ws = `ws://${window.location.hostname}:${window.location.port}/api/chat/ws`;
             const container = document.getElementById('chat-container');
             const socket = new WebSocket(ws);
 
-            auth = JSON.stringify({'token': accessToken, 'message': 'auth'})
+            auth = JSON.stringify({'token': tokenStorage.getToken(), 'message': 'auth'})
             socket.onopen = () => socket.send(auth);
 
             socket.onmessage = function(event) {
@@ -93,6 +135,7 @@ window.addEventListener('hashchange', function(event) {
                 element.setAttribute('style', 'color:red;');
                 container.appendChild(element);
             };
+            enterListener('send-button', 'chat-container');
             document.getElementById('send-button').addEventListener('click', function() {
                 const messageFieldElem = document.getElementById('message-field');
                 const jsonMessage = JSON.stringify({
@@ -102,6 +145,8 @@ window.addEventListener('hashchange', function(event) {
             });
             break;
         case '#register':
+            // Handle registration, send REST API backend request
+            enterListener('register-button', 'register-container');
             window.document.getElementById('register-button').addEventListener('click', function() {
                 const username = document.getElementById('username').value;
                 const password = document.getElementById('password').value;
@@ -121,10 +166,15 @@ window.addEventListener('hashchange', function(event) {
                         if (json.status === 'success') {
                             showStatus('register-container', json.message);
                         }
+                        else {
+                            showStatus('register-container', json.message, 'try again');
+                        }
                     });
             });
             break;
         case '#login':
+            // Handle login, send REST API backend request
+            enterListener('login-button', 'login-container');
             document.getElementById('login-button').addEventListener('click', function() {
                 const username = document.getElementById('username').value;
                 const password = document.getElementById('password').value;
@@ -142,8 +192,11 @@ window.addEventListener('hashchange', function(event) {
                     .then((res) => res.json())
                     .then((json) => {
                         if (json.status === 'success') {
-                            accessToken = json.token;
+                            tokenStorage.setToken(json.token);
                             showStatus('login-container', json.message);
+                        }
+                        else {
+                            showStatus('login-container', json.message, 'try again');
                         }
                     });
             });
